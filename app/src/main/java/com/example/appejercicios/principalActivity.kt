53dac149749cf66eedf888ba73b240d5473d1111
Franco91901 <francoarp15@gmail.com>
2025-09-27 23:09:55 -0500

    login, registro, main y crear rutina. Redireccionamiento basico

---
+++ app/src/main/java/com/example/appejercicios/principalActivity.kt
@@ -0,0 +1,38 @@
+package com.example.appejercicios
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class principalActivity : AppCompatActivity() {
+
+    var btnAgregarRutina : Button ?= null;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_principal)
+
+        btnAgregarRutina = findViewById(R.id.btnAgregarRutina)
+        btnAgregarRutina?.setOnClickListener(){
+            cambioActivity()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun cambioActivity(){
+        val intent =  Intent(this, CrearRutinaActivity::class.java)
+        startActivity(intent)
+    }
+
+}
